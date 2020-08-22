7d405c561340f7e9a56e41497ed8dcb1a86952ac
belaaBel <belugina9898@mail.ru>
2020-08-22 13:19:33 +0300

    slider

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    const mMenuBtn = $(".mobil-button");
+    const mMenu = $(".m-menu");
+    const btn = $(".btn");
+    const actors = $(".actors");
+    const casts = $(".casts");
+
+    mMenuBtn.on("click", function(){
+        mMenu.toggleClass("active");
+        $('body').toggleClass("no-scroll");
+    })
+
+    btn.on("click", () => {
+        btn.toggleClass('active');
+        actors.toggleClass('activ');
+        casts.toggleClass('activ');
+    })
+});
+
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 4,
+    loop: true,
+    spaceBetween: 10,
+    breakpoints: {
+        992: {
+            slidesPerView: 4,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        576: {
+            slidesPerView: 1,
+            navigation: {
+                nextEl: ".next-button",
+            }
+        },
+        376: {
+            slidesPerView: 1,
+        }
+    }
+});
